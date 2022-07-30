912d4d05be3821edf9221ca567de34303c352c45
Brian <brian@briankiernan.com>
2022-07-30 13:42:31 -0400

    start top panel

--- src/pages/dashboard/top-panel/top-panel.tsx
+++ src/pages/dashboard/top-panel/top-panel.tsx
@@ -1,6 +1,21 @@
-import { RuxContainer } from '@astrouxds/react';
+import { RuxContainer, RuxSegmentedButton, RuxSwitch } from '@astrouxds/react';
 import './top-panel.css';
 
 export const TopPanel: React.FC = () => {
-  return <RuxContainer className='Dashboard-top-panel'>Top</RuxContainer>;
+  return (
+    <RuxContainer className='Dashboard-top-panel'>
+      <div className='TopPanel-header' slot='header'>
+        <h2>Center top panel</h2>
+        <div className='TopPanel-header-right'>
+          <p>Show values</p>
+          <RuxSwitch />
+          <RuxSegmentedButton
+            size='small'
+            data={[{ label: 'Table' }, { label: 'Charts', selected: true }]}
+          />
+        </div>
+      </div>
+      <div style={{ height: 192, background: '#333' }}>Charts go here</div>
+    </RuxContainer>
+  );
 };
