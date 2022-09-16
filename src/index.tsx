ca543db53509af0e0c5a3835312f151dbc53221b
Kiley Mitti <kiley.mitti@rocketcom.com>
2022-09-16 16:22:25 -0400

    change location of astro css include

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 
 import App from './app';
 import './index.css';
+import '@astrouxds/astro-web-components/dist/astro-web-components/astro-web-components.css';
 
 const root = document.getElementById('root') as HTMLElement;
 createRoot(root).render(
